ce0fafb0790a15d6d9b7cfc57eebb038792d33ec
Ioannis Manuel Blougouras Cabrera <lblougou@imsalum.com>
2018-05-17 15:41:30 -0500

    first version

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express();
+
+
+app.listen(8000, () => {
+
+    console.log("console.log jajajaj");
+    console.log("app started!!!!");
+    console.log("new console log");
+
+});
