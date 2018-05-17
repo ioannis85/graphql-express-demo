f6be07ffcd4aa6cdf877469b1e01e2dd25658505
Ioannis Manuel Blougouras Cabrera <lblougou@imsalum.com>
2018-05-17 16:35:26 -0500

    servidor graphql

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
-import express from 'express'
+import express from 'express';
+import { graphqlExpress, graphiqlExpress } from 'apollo-server-express';
 
+
+// se inicializa la app de express
 const app = express();
+
+// endpoint que  sirve para realizar las consultas con graphql
+
+app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql'
+}));
+
+
+//app.use('/graphql', graphqlExpress());
 
 
 app.listen(8000, () => {
 
-    console.log("console.log jajajaj");
-    console.log("app started!!!!");
-    console.log("new console log");
+  
 
 });
